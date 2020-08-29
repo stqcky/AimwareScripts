0eda01f05fac7d26770956ef73006075a495e406
stqcky <59932390+stqcky@users.noreply.github.com>
2020-08-29 17:30:15 +0300

    Update DelayedCall.lua

--- DelayedCall.lua
+++ DelayedCall.lua
@@ -1,19 +1,17 @@
 -- Delayed Call by stacky#6580
 
-if not DelayedCall then
-    local delayedCalls = {}
+local delayedCalls = {}
 
-    DelayedCall = function(time, func)
-        table.insert(delayedCalls, {reqTime = common.Time() + time, thread = coroutine.create(func)})
-    end
-
-    callbacks.Register( "Draw", function() 
-        local curTime = common.Time()
-        for i, v in ipairs(delayedCalls) do
-            if curTime >= v.reqTime then
-                coroutine.resume(v.thread)
-                table.remove(delayedCalls, i)
-            end
-        end
-    end)
+DelayedCall = function(time, func)
+    table.insert(delayedCalls, {reqTime = common.Time() + time, thread = coroutine.create(func)})
 end
+
+callbacks.Register( "Draw", function() 
+    local curTime = common.Time()
+    for i, v in ipairs(delayedCalls) do
+        if curTime >= v.reqTime then
+            coroutine.resume(v.thread)
+            table.remove(delayedCalls, i)
+        end
+    end
+end)
