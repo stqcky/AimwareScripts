3146956b7565b6c67a5714fdee373b5eb9629ae4
stqcky <59932390+stqcky@users.noreply.github.com>
2020-09-19 17:02:30 +0300

    fixed

--- BHopHitchance.lua
+++ BHopHitchance.lua
@@ -1,8 +1,8 @@
--- BHop Hitchance by stacky
+-- BHop Hitchance by stacky#6580
 
 local SLIDER = gui.Slider( gui.Reference( "Misc", "Movement", "Jump" ), "hitchance", "Hit Chance", 100, 0, 100 )
 callbacks.Register( "CreateMove", function(cmd)
-    if (gui.GetValue( "misc.autojump" ) ~= '"Off"' or bit.band(cmd.buttons, 2) == 0 or
+    if (gui.GetValue( "misc.autojump" ) ~= 0 or bit.band(cmd.buttons, 2) == 0 or
         bit.band(entities.GetLocalPlayer():GetPropInt("m_fFlags"), 1) == 0 or math.random(1, 100) >= SLIDER:GetValue()) then return end
     cmd.buttons = cmd.buttons - 2
 end )
